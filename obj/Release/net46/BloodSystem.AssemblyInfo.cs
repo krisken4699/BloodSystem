7aef6acbed076b11647d0446b13aa17147f22eee
Ken <krisken4699@gmail.com>
2026-08-06 19:35:02 +0700

    Restore brightness levels to the original 0.70-1.00 range

--- obj/Release/net46/BloodSystem.AssemblyInfo.cs
+++ obj/Release/net46/BloodSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloodSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dc33965d8ca1ddb72392da1d30286a872abb6a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+720967c9abfebf824874186504508ce1bfdc9505")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloodSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloodSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
